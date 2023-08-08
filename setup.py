2cb2822dda271341f6214fdd5cad9a4ae7702ddf
Michael Achmann <michael@achmann.dev>
2023-08-08 17:11:13 +0200

    Added Screenshot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="openai_cost_estimator",
+    name="gpt_cost_estimator",
     version="0.1",
     packages=find_packages(),
     install_requires=[
