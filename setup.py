178cf3769a8f813674c10066328ad1b7a0b06fff
Michael Achmann-Denkler <michael@achmann.dev>
2024-02-15 11:51:09 +0100

    Version Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpt_cost_estimator",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     install_requires=[
         "tiktoken",
